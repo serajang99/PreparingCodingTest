598792c3154fae7a2ec033777e579b08d7597eff
serajang99 <serajang1679@gmail.com>
2021-12-05 01:10:21 +0900

    1205 update

--- 5주차/1548_부분삼각수열.py
+++ 5주차/1548_부분삼각수열.py
@@ -1,8 +1,21 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
 n = int(input().strip())
 a = list(map(int, input().rstrip().split()))
+a = sorted(a)
 
-print(n)
-print(a)
+result = 2
+if n < 3:
+    print(n)
+else:
+    for i in range(n):
+        for j in range(i+2, n):
+            x, y, z = a[i], a[i+1], a[j]
+            cnt = 0
+            if z < x+y:
+                cnt += 1
+            if cnt == 1:
+                result = max(result, j-i+1)
+    print(result)

dc58aecd4de8bbb9d21440e4e877de6fbefd6dba
serajang99 <serajang1679@gmail.com>
2021-12-20 22:04:26 +0900

    1220 update

--- 7주차/2615_오목.py
+++ 7주차/2615_오목.py
@@ -9,33 +9,57 @@ di = [(-1, 1), (0, 1), (1, 0), (1, 1)]
 
 
 def check(y, x):
-    for dy, dx in di:
-        nx = x+dx
-        ny = y+dy
-        if nx < 0 or nx >= 19 or ny < 0 or ny >= 19:
-            return False
+    if x < 0 or x >= 19 or y < 0 or y >= 19:
+        return False
     return True
 
 
-def dfs(y, x, cnt):
-    global answer
-    if cnt == 5:
-        answer = x, cnt
-        return
-    for i in range(x, 19):
-        for j in range(1, 19):
-            if check(i, j):
-                dfs(i, j, cnt+1)
+def count(y, x, val):
+
+    for dy, dx in di:
+        # print("dy dx:", dy, dx)
+        ny, nx = y, x
+        cnt = 1
+        for _ in range(4):
+            nx += dx
+            ny += dy
+            # print(ny, nx, cnt)
+            if check(ny, nx) == True and omok[ny][nx] == val:
+                # print(ny, nx, cnt, omok[ny][nx])
+                cnt += 1
+                if cnt == 5:
+                    # 육목 확인1
+                    if 0 <= nx+dx < 19 and 0 <= ny+dy < 19 and omok[ny+dy][nx+dx] == val:
+                        cnt += 1
+                        break
+                    # 육목 확인2
+                    if 0 <= x-dx < 19 and 0 <= y-dy < 19 and omok[y-dy][x-dx] == val:
+                        cnt += 1
+                        break
+                    return y, x, cnt
+            else:
+                break
+
+    return y, x, cnt
 
 
-answer = -1
+y, x, cnt = -1, -1, 0
+flag = 0
 for i in range(19):
     for j in range(19):
         if omok[i][j] == 1 or omok[i][j] == 2:
             val = omok[i][j]
-            cnt = 1
-            if check(i, j) == True:
-                result = dfs(i, j, cnt)
-                print(result)
+            y, x, cnt = count(i, j, val)
+            # print("result:", y, x, val, cnt)
+            if cnt == 5:
+                print(val)
+                print(y+1, x+1)
+                flag = 1
+                break
         else:
             continue
+    if flag == 1:
+        break
+
+if flag == 0:
+    print(0)

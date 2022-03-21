19b21d5dd91e4224d5b1499cb81aafe38dc62394
serajang99 <serajang1679@gmail.com>
2022-03-21 23:09:17 +0900

    정렬1 문제풀이 완료

--- Programmers/정렬/k번째수.py
+++ Programmers/정렬/k번째수.py
@@ -19,4 +19,9 @@ commands의 각 원소는 길이가 3입니다.
 
 def solution(array, commands):
     answer = []
+    for i, j, k in commands:
+        new_arr = array[i-1:j]
+        new_arr = sorted(new_arr)
+        answer.append(new_arr[k-1])
+
     return answer

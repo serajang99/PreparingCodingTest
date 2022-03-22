1123c8f7762b5bbe442ad63bcf6a1120a7bc338b
serajang99 <serajang1679@gmail.com>
2022-03-22 18:30:30 +0900

    dfsbfs1 문제풀이 완료

---
+++ Programmers/DFSBFS/타겟넘버.py
@@ -0,0 +1,36 @@
+'''
+# 타겟 넘버
+
+## 문제 설명
+n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+## 제한사항
+주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
+각 숫자는 1 이상 50 이하인 자연수입니다.
+타겟 넘버는 1 이상 1000 이하인 자연수입니다.
+'''
+
+
+def bfs(numbers, depth):
+    q = [numbers[0], -numbers[0]]
+
+    while depth != len(numbers)-1:
+        depth += 1
+        q = [x+numbers[depth] for x in q] + [x-numbers[depth] for x in q]
+
+    return q
+
+
+def solution(numbers, target):
+    answer = 0
+    lst = bfs(numbers, 0)
+    for l in lst:
+        if l == target:
+            answer += 1
+    return answer

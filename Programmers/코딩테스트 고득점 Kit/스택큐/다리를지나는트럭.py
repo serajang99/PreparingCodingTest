773ed1dccde79da047d3e8f21b0513de7a2ff45e
serajang99 <serajang1679@gmail.com>
2022-03-30 23:04:42 +0900

    스택큐 문제풀이 완료!

--- Programmers/코딩테스트 고득점 Kit/스택큐/다리를지나는트럭.py
+++ Programmers/코딩테스트 고득점 Kit/스택큐/다리를지나는트럭.py
@@ -2,22 +2,46 @@ from collections import deque
 
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
     truck_weights = deque(truck_weights)
-    bridge = deque([truck_weights.popleft()])
+    bridge = [[truck_weights.popleft(), 1]]
+    answer = 1
     while bridge:
-        cur = truck_weights[0]
-        if sum(bridge)+cur <= weight and len(bridge) <= bridge_length and truck_weights:
-            bridge.append(truck_weights.popleft())
+        if truck_weights:
+            cur = truck_weights[0]
+        else:
+            cur = bridge[0][0]
+        summ = 0
 
-        if len(bridge) > bridge_length:
-            bridge.popleft()
+        for b in bridge:
+            b[1] += 1
 
+        for b in bridge:
+            if b[1] > bridge_length:
+                bridge.pop(0)
         answer += 1
+
+        for w, c in bridge:
+            summ += w
+
+        if summ+cur <= weight:
+            if len(bridge) <= bridge_length and truck_weights:
+                bridge.append([truck_weights.popleft(), 1])
+
+        if len(truck_weights) == 0 and len(bridge) == 0:
+            break
+
     return answer
 
 
-bridge_length = 2
-weight = 10
-truck_weights = [7, 4, 5, 6]
+# bridge_length = 2
+# weight = 10
+# truck_weights = [7, 4, 5, 6]
+
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10]
+
+bridge_length = 100
+weight = 100
+truck_weights = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 print(solution(bridge_length, weight, truck_weights))

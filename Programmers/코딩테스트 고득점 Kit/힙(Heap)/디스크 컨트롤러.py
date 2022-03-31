d3f3347a29174d9057e9c96836c8ec43e3c785b0
serajang99 <serajang1679@gmail.com>
2022-03-31 19:43:13 +0900

    힙2 문제풀이 완료

--- Programmers/코딩테스트 고득점 Kit/힙(Heap)/디스크 컨트롤러.py
+++ Programmers/코딩테스트 고득점 Kit/힙(Heap)/디스크 컨트롤러.py
@@ -6,18 +6,19 @@ def solution(jobs):
     answer = 0
     cur = 0
     n = len(jobs)
-    jobs = deque(sorted(jobs, key=lambda x: x[1]))
+    jobs = deque(sorted(jobs, key=lambda x: x[0]))
     heap = []
     while len(jobs) != 0 or len(heap) != 0:
-        while len(jobs) != 0 and jobs[0][0] <= cur:
+
+        while jobs and jobs[0][0] <= cur:
             heapq.heappush(heap, jobs.popleft()[::-1])
 
         if len(heap) == 0:
             cur = jobs[0][0]
             continue
 
-        process = heapq.heappop(heap)
-        cur += process[0]
-        answer += cur - process[1]
+        time, start = heapq.heappop(heap)
+        cur += time
+        answer += cur - start
 
     return answer // n

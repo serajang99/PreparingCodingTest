85c2f73d70ee905a8d30877b9fe538cfd42eb8db
Jang Seowoo <41984266+serajang99@users.noreply.github.com>
2023-10-22 00:04:41 +0900

    [level 2] Title: 전화번호 목록, Time: 190.61 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
+    return True
